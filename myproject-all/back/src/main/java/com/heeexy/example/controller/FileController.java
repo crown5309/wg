ac478effb5021f06ebef8265e03c5ceb4900ca03
zlbg <1450336681@qq.com>
2020-06-19 10:56:24 +0800

    xss防御

--- myproject-all/back/src/main/java/com/heeexy/example/controller/FileController.java
+++ myproject-all/back/src/main/java/com/heeexy/example/controller/FileController.java
@@ -2,7 +2,9 @@ package com.heeexy.example.controller;
 
 import java.io.File;
 import java.io.FileOutputStream;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -23,27 +25,32 @@ public class FileController {
 	@Value("${imgPath}")
 	private String imgPath;
 	@RequestMapping(value="/uploadimg", method = RequestMethod.POST)
-    public @ResponseBody JSONObject uploadImg(@RequestParam("file") MultipartFile file,
+    public @ResponseBody JSONObject uploadImg(@RequestParam("file") MultipartFile[] file,
             HttpServletRequest request) {
 		String scheme = request.getScheme();//http
 		String serverName = request.getServerName();//localhost
 		int serverPort = request.getServerPort();//8080
 		String contextPath = request.getContextPath();//项目名
 		String url = scheme+"://"+serverName+":"+serverPort+contextPath;//http://127.0.0.1:8080/test
-        String contentType = file.getContentType();
-        String fileName = file.getOriginalFilename();
-        /*System.out.println("fileName-->" + fileName);
-        System.out.println("getContentType-->" + contentType);*/
-        String format = DateUtil.format(new Date(), "yyyMMdd")+"/";
-		String filePath = imgPath+format;
-		String substring = OrderIdFactory.getOrderIdByUUId()+fileName.substring(fileName.lastIndexOf("."));
-        try {
-			uploadFile(file.getBytes(), filePath,substring);
-        } catch (Exception e) {
-            // TODO: handle exception
-        }
+		List<String> fiList=new ArrayList<String>();
+		for(MultipartFile f:file) {
+			 String contentType = f.getContentType();
+		        String fileName = f.getOriginalFilename();
+		        /*System.out.println("fileName-->" + fileName);
+		        System.out.println("getContentType-->" + contentType);*/
+		        String format = DateUtil.format(new Date(), "yyyMMdd")+"/";
+				String filePath = imgPath+format;
+				String substring = OrderIdFactory.getOrderIdByUUId()+fileName.substring(fileName.lastIndexOf("."));
+		        try {
+					uploadFile(f.getBytes(), filePath,substring);
+					fiList.add(url+"/img"+"/"+format+substring);
+		        } catch (Exception e) {
+		            // TODO: handle exception
+		        }
+		}
+       
         //返回json
-        return CommonUtil.successJson(url+"/img"+"/"+format+substring);
+        return CommonUtil.successJson(fiList);
     }
 	public static void uploadFile(byte[] file, String filePath, String fileName) throws Exception { 
         File targetFile = new File(filePath);  
