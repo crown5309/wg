b8769ae8b6ab69ee404c4c9e9dfc69726b60aad9
zlbg <1450336681@qq.com>
2020-06-29 15:27:00 +0800

    定时任务优化

--- myproject-all/back/src/main/java/com/heeexy/example/task/AutoTask.java
+++ myproject-all/back/src/main/java/com/heeexy/example/task/AutoTask.java
@@ -21,14 +21,14 @@ public class AutoTask {
 	private OrderDao orderDao;
 //	@Autowired
 	//private GoodsDao goodsDao;
-	@Scheduled(cron = "0 0/10 0 * * ?") // 订单取消 （1.还库存 ）
+	@Scheduled(cron = "0 0/10 * * * ?") // 订单取消
 	private void process() {
 		log.info("*************取消订单定时任务start***************");
 		List<OrderInfo> list = orderDao.getOrderInfoByNoPay(1);
 	//	List<JSONObject> goodsList = null;
 		long chaTime = 1000 * 60 * 60 * 1;//10分为支付取消订单
 		for (OrderInfo sg : list) {// 取消订单
-			if (new Date().getTime() - sg.getLong("createTime") > chaTime) {
+			if (new Date().getTime() - sg.getDate("createTime").getTime() > chaTime) {
 				orderDao.cancelOrderState(sg.getString("orderId"), 0);
 			//	goodsList = (List<JSONObject>) sg.get("goodsList");
 //				for (JSONObject goods : goodsList) {
